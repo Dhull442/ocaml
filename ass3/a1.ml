721d8a293f50d9949917c45e9045bdee31604c6e
Saksham Dhull <sakshamdhull442@gmail.com>
2019-03-06 13:40:44 +0530

    ass3 mid

---
+++ ass3/a1.ml
@@ -0,0 +1,174 @@
+open A0
+(* code goes here *)
+type answer = Num of bigint | Bool of bool | Tup of int * (answer list) | Var of string;;
+
+type exptree = Done
+  | N of int (* Integer constant *)
+  | B of bool (* Boolean constant *)
+  | Var of string (* variable *)
+  | Conjunction of exptree * exptree (* binary operators on booleans /\ *)
+  | Disjunction of exptree * exptree (* binary operators on booleans \/ *)
+  | Not of exptree
+  | Equals of exptree * exptree      (* comparison operations on integers *)
+  | GreaterTE of exptree * exptree   (* comparison operations on integers *)
+  | LessTE of exptree * exptree      (* comparison operations on integers *)
+  | GreaterT of exptree * exptree    (* comparison operations on integers *)
+  | LessT of exptree * exptree       (* comparison operations on integers *)
+  | InParen of exptree               (* expressions using parenthesis *)
+  | IfThenElse of exptree * exptree * exptree (* a conditional expression *)
+  | Tuple of int * (exptree list)           (* creating n-tuples (n >= 0) *)
+  | Project of (int*int) * exptree          (* projecting the i-th component of an expression (which evaluates to an n-tuple, and 1 <= i <= n) *)
+  | Plus of exptree * exptree        (* binary operators on integers *)
+  | Minus of exptree * exptree       (* binary operators on integers *)
+  | Mult of exptree * exptree        (* binary operators on integers *)
+  | Div of exptree * exptree         (* binary operators on integers *)
+  | Rem of exptree * exptree         (* binary operators on integers *)
+  | Nega of exptree       (* unary operators on booleans *)
+  | Abs of exptree;;        (* unary operators on integers *)
+
+type opcode = NCONST of bigint | PLUS | TIMES | MINUS | DIV | REM | ABS | UNARYMINUS
+    | EQS | GTE | LTE | GT | LT | PAREN
+    | BCONST of bool | CONJ | DISJ | NOT
+    | IFTE | TUPLE of int | PROJ of int;;
+
+(* Excpetions *)
+exception InvalidArgument;;
+exception IndexOutOfBound;;
+(* Eval function using simple recursive calls *)
+let rec eval t =
+  (* Provides +ve modulo  *)
+  let rec modulo a1 b1 =
+    if a1 < 0 then modulo ( a1 + b1 ) b1
+    else a1 mod b1
+  in
+  (* Provides accurate quotient with +ve modulo *)
+  let rec divide a1 b1 =
+    if a1 < 0 then ( divide ( a1 + b1 ) b1 ) - 1
+    else a1 / b1
+  in
+  (* Unwrap bigint *)
+  let nunwrap a = match a with
+    Num a1 -> a1
+    | _ -> raise InvalidArgument
+  in
+  (* unwrap booleans *)
+  let bunwrap b = match b with
+    Bool b1 -> b1
+    | _ -> raise InvalidArgument
+  in
+  (* mapping function *)
+  let rec map func a = match a with
+    [] -> []
+  |  li :: ls -> (func li) :: (map func ls)
+  in
+  (* get ith element *)
+  let rec get i alist = match alist with
+   li::ls -> (if i > 1 then get (i-1) ls else li)
+  | _ -> raise IndexOutOfBound
+  in
+  match t with
+      Done -> raise InvalidArgument
+    | N number -> Num (mk_big number)
+    | B boolean -> Bool boolean
+    | Var str -> Var (str)
+    | Conjunction (a,b) -> Bool ((bunwrap (eval a))||(bunwrap (eval b)))
+    | Disjunction (a,b) -> Bool ((bunwrap (eval a))&&(bunwrap (eval b)))
+    | Not t -> Bool ( not (bunwrap (eval t) ) )
+    | Equals (a,b) ->  Bool (eq (nunwrap (eval a)) (nunwrap (eval b)))
+    | GreaterTE (a,b) -> Bool (geq (nunwrap (eval a)) (nunwrap (eval b)))
+    | LessTE (a,b) -> Bool (leq (nunwrap (eval a)) (nunwrap (eval b)))
+    | GreaterT (a,b) -> Bool (gt (nunwrap (eval a)) (nunwrap (eval b)))
+    | LessT (a,b) -> Bool (lt (nunwrap (eval a)) (nunwrap (eval b)))
+    | InParen a -> eval a
+    | IfThenElse (cond,caseT,caseF) -> if (bunwrap (eval cond)) then (eval caseT) else (eval caseF)
+    | Tuple ( intg , explist ) -> Tup ( intg , map eval explist )
+    | Project ((i,n),tree) -> (match (eval tree) with Tup (size, alist ) -> (if ((i <= n) && (n <= size)) then get i alist else raise InvalidArgument) | _ -> raise InvalidArgument)
+    | Plus ( a , b ) -> Num (add (nunwrap (eval a) ) (nunwrap (eval b) ))
+    | Minus ( a , b ) -> Num (sub (nunwrap (eval a) ) (nunwrap (eval b) ))
+    | Mult ( a , b ) -> Num (mult (nunwrap (eval a) ) (nunwrap (eval b) ))
+    | Div ( a , b ) -> Num (div (nunwrap (eval a) ) (nunwrap (eval b) ))
+    | Rem ( a , b ) -> Num (rem (nunwrap (eval a) ) (nunwrap (eval b) ))
+    | Nega a -> Num (minus (nunwrap (eval a) ))
+    | Abs a -> Num (abs (nunwrap (eval a))) ;;
+(* val eval : exptree -> answer *)
+
+(* Stackmc function with tailrecursion *)
+let rec stackmc bl ol =
+(* Unwrap bigint *)
+let nunwrap a = match a with
+  Num a1 -> a1
+  | _ -> raise InvalidArgument
+in
+(* unwrap booleans *)
+let bunwrap b = match b with
+  Bool b1 -> b1
+  | _ -> raise InvalidArgument
+in
+(* Extract top n elements from stack *)
+let rec extractlist n ansstack stack = if ( n <= 0 ) then (ansstack,stack) else
+match stack with
+si :: ss -> extractlist (n-1) (si :: ansstack) ss
+| [] -> raise InvalidArgument
+in
+(* get ith element *)
+let rec get i alist = match alist with
+ li::ls -> (if i > 1 then get (i-1) ls else li)
+| _ -> raise IndexOutOfBound
+in
+match ol with
+  [] -> List.hd bl
+| (NCONST a) :: ols-> stackmc ( (Num a) :: bl ) ols
+| PLUS :: ols -> ( match bl with el2 :: ( el1 :: bls ) -> stackmc ( ( Num (add (nunwrap el1) (nunwrap el2) )) :: bls ) ols | _ -> raise InvalidArgument )
+| MINUS :: ols -> ( match bl with el2 :: ( el1 :: bls ) -> stackmc ( (Num ( sub (nunwrap el1) (nunwrap el2) )) :: bls ) ols | _ -> raise InvalidArgument )
+| TIMES :: ols -> ( match bl with el2 :: ( el1 :: bls) -> stackmc ( (Num ( mult (nunwrap el1) (nunwrap el2) )) :: bls ) ols | _ -> raise InvalidArgument )
+| DIV :: ols -> ( match bl with el2 :: ( el1 :: bls) -> stackmc ( (Num ( div (nunwrap el1) (nunwrap el2) )) :: bls ) ols | _ -> raise InvalidArgument )
+| REM :: ols -> ( match bl with el2 :: ( el1 :: bls) -> stackmc ( (Num ( rem (nunwrap el1) (nunwrap el2) )) :: bls ) ols | _ -> raise InvalidArgument )
+| ABS :: ols -> ( match bl with el1 :: bls -> stackmc ( (Num ( abs (nunwrap el1) )) :: bls ) ols | _ -> raise InvalidArgument )
+| UNARYMINUS :: ols -> ( match bl with el1 :: bls-> stackmc ( (Num ( minus (nunwrap el1) )) :: bls ) ols | _ -> raise InvalidArgument );;
+| EQS :: ols -> ( match bl with el2 :: ( el1 :: bls ) -> stackmc ( ( Bool (eq (nunwrap el1) (nunwrap el2)) ) :: bls ) ols | _ -> raise InvalidArgument )
+| GTE :: ols -> ( match bl with el2 :: ( el1 :: bls ) -> stackmc ( ( Bool (geq (nunwrap el1) (nunwrap el2)) ) :: bls ) ols | _ -> raise InvalidArgument )
+| LTE :: ols -> ( match bl with el2 :: ( el1 :: bls ) -> stackmc ( ( Bool (leq (nunwrap el1) (nunwrap el2)) ) :: bls ) ols | _ -> raise InvalidArgument )
+| GT :: ols -> ( match bl with el2 :: ( el1 :: bls ) -> stackmc ( ( Bool (gt (nunwrap el1) (nunwrap el2)) ) :: bls ) ols | _ -> raise InvalidArgument )
+| LT :: ols -> ( match bl with el2 :: ( el1 :: bls ) -> stackmc ( ( Bool (lt (nunwrap el1) (nunwrap el2)) ) :: bls ) ols | _ -> raise InvalidArgument )
+| PAREN :: ols -> raise InvalidArgument
+| (BCONST b) :: ols -> stackmc ((Bool b) :: bl) ols
+| CONJ :: ols -> (match bl with el2 :: (el1 :: bls) -> stackmc ( ( Bool ( (bunwrap el1) || (bunwrap el2) ) ) :: bls ) ols | _ -> raise InvalidArgument)
+| DISJ :: ols -> (match bl with el2 :: (el1 :: bls) -> stackmc ( ( Bool ( (bunwrap el1) && (bunwrap el2) ) ) :: bls ) ols | _ -> raise InvalidArgument)
+| NOT :: ols -> (match bl with el :: bls -> stackmc ((Bool (not (bunwrap el)))::bls) ols| _ -> raise InvalidArgument)
+| IFTE :: ols -> (match bl with caseF :: ( caseT :: ( cond :: bls ) ) ->( if (bunwrap cond) then stackmc (caseT :: bls) ols else stackmc (caseF :: bls) ols)| _ -> raise InvalidArgument)
+| (TUPLE a) :: ols -> (let tmp = extractlist a [] bl in stackmc (( Tup (a , (get 1 tmp)  ) ) :: (get 2 tmp)) ols)
+| PROJ which -> (match bl with Tup(a,alist)::bls -> stackmc ((get which alist)::bls) ols | _ -> raise InvalidArgument )
+| _ -> raise InvalidArgument;;
+(* val stackmc: (bigint list) -> (opcode list) -> bigint *)
+
+(* Tail rec compilation *)
+let rec compile t =
+  let rec map func a = match a with
+    li :: ls -> (func li) :: (map func ls)
+    | [] -> []
+    | _ -> raise InvalidArgument
+  in
+  match t with
+    N a -> [NCONST ( mk_big a )]
+  | B b -> [BCONST b]
+  (* | Var str -> [] *)
+  | Conjunction (a,b) -> (compile a ) @ (compile b) @ [ CONJ ]
+  | Disjunction (a,b) -> (compile a ) @ (compile b) @ [ DISJ ]
+  | Not a -> (compile a )@ [ NOT ]
+  | Equals (a,b) -> (compile a ) @ (compile b) @ [EQS]
+  | GreaterTE (a,b) -> (compile a ) @ (compile b) @ [GTE]
+  | LessTE (a,b) -> (compile a ) @ (compile b) @ [LTE]
+  | GreaterT (a,b) -> (compile a ) @ (compile b) @ [GT]
+  | LessT (a,b) -> (compile a) @ (compile b) @ [LT]
+  | InParen (a) -> (compile a) @ [ PAREN ]
+  | IfThenElse (cond,caseT,caseF) -> (compile cond) @ (compile caseT) @ (compile caseF)  @ [IFTE]
+  | Tuple (a,explist) -> (map compile explist) @ [ TUPLE a ]
+  | Project ((i,n),tree) -> (compile tree) @ [ PROJ i ]
+  | Plus ( a , b ) -> ( compile a ) @ ( compile b ) @ [ PLUS ]
+  | Minus ( a , b ) -> ( compile a ) @ ( compile b ) @ [ MINUS ]
+  | Mult ( a , b ) -> ( compile a ) @ ( compile b ) @ [ TIMES ]
+  | Div ( a , b ) -> ( compile a ) @ ( compile b ) @ [ DIV ]
+  | Rem ( a , b ) -> ( compile a ) @ ( compile b ) @ [ REM ]
+  | Nega a -> ( compile a ) @ [ UNARYMINUS ]
+  | Abs a -> ( compile a ) @ [ ABS ];;
+(* val compile: exptree -> opcode list *)
---
+++ ass3/a2.mll
@@ -0,0 +1,109 @@
+ (* TOKEN definition *)
+{
+  type token =
+     INT of int          (* integer constant, positive or negative w/o leading zeros *)
+  |  TRUE                (* boolean constant "T" *)
+  |  FALSE               (* boolean constant "F" *)
+  |  ABS                 (* unary operator, "abs" *)
+  |  NEG                 (* Unary Minus *)
+  |  PLUS                (* arithmetic plus, "+" *)
+  |  MINUS               (* arithmetic minus, "-" *)
+  |  MUL                 (* arithmetic multiply, "*" *)
+  |  DIV                 (* integer div, "div" *)
+  |  MOD                 (* remainder, "mod" *)
+  (* |  EXP                 (* exponentiation, "^" *) *)
+  |  LP                  (* left paren, "(" *)
+  |  RP                  (* right paren, ")" *)
+  |  NOT                 (* boolean NOT, "not" *)
+  |  AND                 (* boolean AND, "/\ " *)
+  |  OR                  (* boolean OR, "\/" *)
+  |  EQ                  (* equal to, "=" *)
+  |  GTA                 (* greater than, ">" *)
+  |  LTA                 (* less than, "<" *)
+  |  GEQ                 (* greater than/equal to, ">=" *)
+  |  LEQ                 (* less than/equal to, "<=" *)
+  |  IF                  (* keyword "if" *)
+  |  THEN                (* keyword "then" *)
+  |  ELSE                (* keyword "else" *)
+  |  FI                  (* keyword "fi" *)
+  |  ID of string        (* variable identifier, alphanumeric string with first char UPPERcase *)
+  (* |  DEF                 (* definition construct, "def" *) *)
+  (* |  DELIMITER           (* delimiter, ";" *) *)
+  |  ENDINPUT;;          (* command for end of input signature *)
+  exception InvalidToken of char;;
+}
+
+(* REGEX PATTERNS *)
+
+let whitespace = [' ' '\n' '\t']+
+let digit = ['0'-'9']
+let integer = ['-']?('0'|['1'-'9']digit*)
+let alphanum = ['A'-'Z']['A'-'Z' 'a'-'z' '0'-'9' '_' '\'']*
+
+(* RULE *)
+rule read = parse
+  whitespace            { read lexbuf }
+| integer as n          { INT (int_of_string n) }
+| 'T'                   { TRUE }
+| 'F'                   { FALSE }
+| 'a''b''s'             { ABS }
+| '~'                   { NEG }
+| '+'                   { PLUS }
+| '-'                   { MINUS }
+| '*'                   { MUL }
+| 'd''i''v'             { DIV }
+| 'm''o''d'             { MOD }
+| '('                   { LP }
+| ')'                   { RP }
+| 'n''o''t'             { NOT }
+| '/''\\'               { AND }
+| '\\''/'               { OR }
+| '>''='                { GEQ }
+| '<''='                { LEQ }
+| '='                   { EQ }
+| '>'                   { GTA }
+| '<'                   { LTA }
+| 'i''f'                { IF }
+| 't''h''e''n'          { THEN }
+| 'e''l''s''e'          { ELSE }
+| 'f''i'                { FI }
+| alphanum as s         { ID s }
+| eof                   { ENDINPUT }
+| _ as invalid {raise (InvalidToken invalid)}
+(* | 'd''e''f'             { DEF } *)
+(* | '^'                   { EXP } *)
+(* | ';'                   { DELIMITER } *)
+
+
+(* FUNCTIONS *)
+{
+  (* Function to read recursively *)
+  let rec readwhole lexbuf readlist = match read lexbuf with
+    ENDINPUT -> readlist
+  | readval  -> readwhole lexbuf (readlist @ [readval] );;
+
+  let scanner s = readwhole (Lexing.from_string s) [];;
+}
+
+
+(* EXAMPLES *)
+(* scanner "";; - : token list = [] *)
+(* scanner "123 -123";; - : token list = [INT 123; INT (-123)] *)
+(* scanner "123 - 123";; - : token list = [INT 123; MINUS; INT 123] *)
+(* scanner "T /\\ F";; - : token list = [TRUE; AND; FALSE] *)
+(* scanner "vAr001=120";; - : token list = [ID "sEAd"; EQ; INT 120] *)
+(* scanner "if (cat = 10) then T else (24*7 + 23)";; - : token list = [IF; LP; ID "cat"; EQ; INT 10; RP; THEN; TRUE; ELSE; LP; INT 24; MUL; INT 7; PLUS; INT 23; RP] *)
+(* scanner ">==<";; - : token list = [GEQ; EQ; LTA] *)
+(* scanner "modnot";; - : token list = [ID "modnot"] *)
+(* scanner "mod not";; - : token list = [MOD; NOT] *)
+(* canner "10 + -20 - 10*230div-2";; - : token list = [INT 10; PLUS; INT (-20); MINUS; INT 10; MUL; INT 230; DIV; INT (-2)] *)
+(* scanner "def >= 10";; - : token list = [DEF; GEQ; INT 10] *)
+(* scanner " notT ";; - : token list = [ID "notT"] *)
+(* scanner "+192 --1";; - : token list = [PLUS; INT 192; MINUS; INT (-1)] *)
+(* scanner "000";; - : token list = [INT 0; INT 0; INT 0] *)
+(* scanner "i(f>=<";; - : token list = [ID "i"; LP; ID "f"; GEQ; LTA] *)
+(* scanner "if>=<";; - : token list = [IF; GEQ; LTA] *)
+(* scanner "10div2";; - : token list = [INT 10; ID "div2"] *)
+(* scanner "div;";; - : token list = [DIV; DELIMITER] *)
+(* scanner ";()mod;
+;;;;+10;";; - : token list = [DELIMITER; LP; RP; MOD; DELIMITER; DELIMITER; DELIMITER; DELIMITER; DELIMITER; PLUS; INT 10; DELIMITER] *)
