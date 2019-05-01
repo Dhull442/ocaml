dde099ca4019d19f739b95e0c35839cd006861dd
Saksham Dhull <sakshamdhull442@gmail.com>
2019-05-01 23:41:15 +0530

    error handling and front end

--- a6/com.ml
+++ a6/com.ml
@@ -3,13 +3,19 @@ open Parser
 open Lexer
 let ans = ref Main.ans;;
 let _ =
+print_endline "_______________________________________________________________________";
+print_endline "| SYNTAX | call P (a,b) | pointers | view | a := 10 | a := b | return | ";
+print_endline "|________|______________|__________|______|_________|________|________|";
 Printf.printf ":~> ";flush stdout;
     let lexbuf = Lexing.from_channel stdin in
         while true do
         try
         let result = Parser.comd Lexer.read lexbuf in
         ans := Main.eval result !ans ;
-        Printf.printf "\n:~> "; flush stdout
+        print_endline "_______________________________________________________________________";
+        print_endline "| SYNTAX | call P (a,b) | pointers | view | a := 10 | a := b | return | ";
+        print_endline "|________|______________|__________|______|_________|________|________|";
+        Printf.printf ":~> ";flush stdout;
         (* flush ensures that the evaluated information gets printed to stdout *)
         with Main.Error s ->
           print_bytes ("EXCEPTION: "^s^" \n:~> ") ; flush stdout
